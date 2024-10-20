55690822123a26db7fbb220f1e06cdacbb48d078
nafa39 <nafatulmiskiyah@gmail.com>
2024-10-21 05:19:24 +0700

    masih susah banget cokk ini livecode2

--- Phase 0/Week 2/[Day 4] Algoritma + Pseudocode/Kisi-kisi Livecode 2/script.js
+++ Phase 0/Week 2/[Day 4] Algoritma + Pseudocode/Kisi-kisi Livecode 2/script.js
@@ -39,7 +39,185 @@ function calculateAverage(grade){
     console.log(avg);
 }
 
-console.log(calculateAverage(facultyGrades2));
+calculateAverage(facultyGrades2);
 
 //Soal 2
 
+function checkKeyword(keyword){
+    let word = keyword;
+    let arrayKeyword = word.split(' ');
+    if (arrayKeyword.length<2){
+        return false;
+    }
+    else{
+        let chara = [];
+        for (let i=0; i<arrayKeyword.length; i++){
+            let character = arrayKeyword[i].split("");
+            chara.push(character);
+            }
+        
+        for (let j=0; j<chara.length;j++){
+            if (chara[j].length<4){
+                return false;
+            }
+        }
+    }
+    for (let x=0; x<arrayKeyword.length;x++){
+        const pattern = /[0-9~`!@#$%\^&*()+=\-\[\]\\';,/{}|\\":<>\?¬£.]+/;
+        if (pattern.test(arrayKeyword[x])){
+            //console.log('contains special chara');
+            return false;
+        }else{
+            //console.log('not contains special chara');
+            return true;
+        }
+    }
+    
+}
+
+//console.log(checkKeyword("Harry Pottter"));
+
+const libraryData = {
+    publisher: "Bloomsburry",
+    books: [
+        "Harry Potter and the Philosoper's Stone",
+        "Harry Potter and the Chamber of Secrets",
+        "Harry Potter and the Prisoner of Azkaban",
+        "The Casual Vacancy",
+        "A Cuckoo's Calling",
+        "The Silkworm"
+    ]
+}
+
+function bookFinder(libraryData, keyword){
+    checkKeyword(keyword);
+    if (checkKeyword(keyword)){
+        let key = keyword.toLowerCase().split(' ');
+        let foundBooks = [];
+        for (let i=0; i<libraryData.books.length; i++){
+            let bookWords = libraryData.books[i].toLowerCase().split(' ');
+            let match = key.every(word => bookWords.includes(word));
+            if (match){
+                foundBooks.push(libraryData.books[i]);
+            }
+        }
+
+        console.log ([libraryData.publisher, [foundBooks]]);
+    }else{
+        console.log('Keyword not suitable');
+    }
+}
+
+bookFinder(libraryData, "Harry Potter");
+
+//Soal 3
+
+const armorsAvailable = [
+    "Dragon Scale",
+    "Elven Leather",
+    "Chainmail"
+]
+
+function getArmorAvailability (armors){
+    let armorsAvail = [];
+    for (let i=0; i<armorsAvailable.length; i++){
+        if (armors === armorsAvailable[i]){
+            armorsAvail.push(armorsAvailable[i]);
+            console.log(`"${armorsAvail}": true`);
+            return armorsAvail;
+        }
+    }
+    console.log(`"${armors}": false`);
+}
+getArmorAvailability("Chainmail");
+
+const armors = [
+    ['Dragon Scale', 'Heavy', 50],
+    ['Elven Leather', 'Light', 30],
+    ['Wizard Robe', 'Cloth', 15],
+    ['Chainmail', 'Medium', 40]
+]
+
+const character1 = {
+    name: 'Lorian',
+    classType: 'Knight',
+    baseDefense: 100,
+    armorUsed: ['Elven Leather', 'Chainmail']
+}
+
+function getArmorAv1ailability1 (armors1){
+    let armorsAvail = [];
+    for (let i=0; i<armors.length; i++){
+        if (armors1 === armors[i][0]){
+            armorsAvail.push(armors[i][0]);
+            console.log(`"${armorsAvail}": ${true}`);
+            return armors[i][2];
+        }
+    }
+    console.log(`"${armors1}": ${false}`);
+    //return index;
+}
+
+function getTotalDefense(character){
+    let total = character.baseDefense;
+    for (let i=0; i<character.armorUsed.length; i++){
+        let addedDefense = getArmorAv1ailability1(character.armorUsed[i])
+        total += addedDefense;
+    }
+    console.log(total);
+}
+
+getTotalDefense(character1);
+
+const inputCharacters = [
+    {
+        character: {
+            name: 'Lorian',
+            classType: 'Knight',
+            baseDefense: 100,
+            armorUsed: []
+        },
+        armorToEquip: ['Elven Leather', 'Chainmail']
+    },
+];
+
+const armorsInArmory = [
+    "Dragon Scale",
+    "Elven Leather",
+    "Chainmail",
+    "Wizard Robe"
+]
+
+function getArmorAvailability2 (armors){
+    let armorsAvail = [];
+    for (let i=0; i<armors.length; i++){
+        if (armors1 === armors[i][0]){
+            armorsAvail.push(armors[i][0]);
+            console.log(`"${armorsAvail}": ${true}`);
+            return armors[i][2];
+        }
+    }
+    console.log(`"${armors1}": ${false}`);
+}
+
+function getTotalDefense(character){
+    let total = character.baseDefense;
+    for (let i=0; i<character.armorUsed.length; i++){
+        let addedDefense = getArmorAv1ailability2(character.armorUsed[i])
+        total += addedDefense;
+    }
+    console.log(total);
+}
+
+function getDefenseSummary (inputCharacters, armorsInArmory){
+    for (let i=0; i<inputCharacters.length; i++){
+        for (let j=0; j< inputCharacters[i].armorToEquip.length; j++){
+            let arUsed = getArmorAvailability2 (inputCharacters[i].armorToEquip[j])
+            inputCharacters[i].character[j].armorUsed.push(arUsed);
+            return arUsed;
+        }
+        getTotalDefense(inputCharacters[i].character);
+    }
+}
+
+getDefenseSummary(inputCharacters, armorsAvailable);
