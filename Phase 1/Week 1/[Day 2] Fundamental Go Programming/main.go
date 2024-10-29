0df8f51ce3822b100ef1f9cd4cc2e93d45d48723
nafa39 <nafatulmiskiyah@gmail.com>
2024-10-29 19:19:20 +0700

    update phase 1

---
+++ Phase 1/Week 1/[Day 2] Fundamental Go Programming/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Airell"
+	var age int = 23
+
+	fmt.Println("Ini namanya adalah ==>", name)
+	fmt.Println("Ini adalah umurnya ==>", age)
+}
