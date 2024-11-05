cf04ebaa8222036445f7c025e4f408cdd2e137ab
nafa39 <nafatulmiskiyah@gmail.com>
2024-11-05 22:02:22 +0700

    learn go routine

---
+++ Phase 1/Week 2/[Day 2] Go Routines/image.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func processImage(imageURL string) {
+	fmt.Printf("Processing image %s\n", imageURL)
+
+	//simulate image processing
+	time.Sleep(3 * time.Second)
+	fmt.Printf("Image processing completed %s\n", imageURL)
+}
+
+func main() {
+	images := []string{
+		"https://example.com/image1.jpg",
+		"https://example.com/image2.jpg",
+		"https://example.com/image3.jpg",
+		"https://example.com/image4.jpg",
+		"https://example.com/image5.jpg",
+	}
+
+	for _, imageURL := range images {
+		go processImage(imageURL)
+	}
+
+	fmt.Println("Image processing started, main application continues ...")
+
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("All image processing completed")
+}
---
+++ Phase 1/Week 2/[Day 2] Go Routines/main.go
@@ -0,0 +1,129 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+
+// // func routine1() {
+// // 	for i := 0; i < 5; i++ {
+// // 		fmt.Println("routine: ", i)
+// // 		time.Sleep(1 * time.Second)
+// // 	}
+// // }
+
+// //go routine send email
+
+// func sendEmail() {
+// 	for i := 0; i < 2; i++ {
+// 		fmt.Println("Sending email: ", i)
+// 		time.Sleep(3 * time.Second)
+// 	}
+// }
+
+// func sendSMS() {
+// 	for i := 0; i < 2; i++ {
+// 		fmt.Println("Sending SMS: ", i)
+// 		time.Sleep(2 * time.Second)
+// 	}
+// }
+
+// func storeData() {
+// 	for i := 0; i < 5; i++ {
+// 		fmt.Println("Storing data: ", i)
+// 		time.Sleep(1 * time.Second)
+// 	}
+// }
+
+// func main() {
+// 	//start a go routine
+// 	// go routine1()
+
+// 	// //main routine
+// 	// for i := 0; i < 2; i++ {
+// 	// 	fmt.Println("main: ", i)
+// 	// 	//time.Sleep(1 * time.Second)
+// 	// }
+
+// 	// time.Sleep(5 * time.Second)
+// 	// fmt.Println("main function")
+
+// 	var wg sync.WaitGroup
+// 	wg.Add(2)
+
+// 	//go routine
+// 	go func() {
+// 		go sendEmail()
+// 		wg.Done()
+// 	}()
+
+// 	go func() {
+// 		go sendSMS()
+// 		wg.Done()
+// 	}()
+
+// 	wg.Wait()
+
+// 	storeData()
+
+// 	time.Sleep(6 * time.Second)
+// }
+
+// go routine to send email
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+
+// func sendEmail() {
+// 	for i := 0; i < 2; i++ {
+// 		// SMTP code to send email
+// 		time.Sleep(3 * time.Second)
+// 		fmt.Println("Sending email: ", i)
+// 	}
+// }
+
+// func sendSMS() {
+// 	for i := 0; i < 2; i++ {
+// 		// SMS code to send SMS
+// 		time.Sleep(2 * time.Second)
+// 		fmt.Println("Sending SMS: ", i)
+// 	}
+// }
+
+// func storeData() {
+// 	for i := 0; i < 5; i++ {
+// 		// Database code to store data
+// 		time.Sleep(1 * time.Second)
+// 		fmt.Println("Storing data: ", i)
+// 	}
+// }
+
+// func main() {
+// 	var wg sync.WaitGroup
+
+// 	wg.Add(2)
+
+// 	go func() {
+// 		sendEmail()
+// 		wg.Done()
+// 	}()
+
+// 	go func() {
+// 		sendSMS()
+// 		wg.Done()
+// 	}()
+
+// 	wg.Wait()
+
+// 	storeData()
+
+// 	time.Sleep(6 * time.Second)
+// }
+
+//CATATAN
+//asynchronous programming pasti delay dulu
