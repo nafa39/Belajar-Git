a265f1a0d840e6d7ed7c183ef0c0770a04806dfd
nafa39 <nafatulmiskiyah@gmail.com>
2024-11-20 16:23:18 +0700

    update p0w2d3

--- Phase 0/Week 2/[Day 3] Javascript Part 2/script.js
+++ Phase 0/Week 2/[Day 3] Javascript Part 2/script.js
@@ -21,27 +21,99 @@ else{
 
 //let list = [];
 
-let lowLimit = parseInt(prompt('Masukkan limit bawah:'));
-let highLimit = parseInt(prompt('Masukkan limit atas:'));
+// let lowLimit = parseInt(prompt('Masukkan limit bawah:'));
+// let highLimit = parseInt(prompt('Masukkan limit atas:'));
 
-function bilPrima(n){
-    if(n<=1){
-        return false;
-    }
-    for (let i=2; i<=n-1;i++){
-        if (n % i === 0){
-            return false;
-        }
-    }
-    return true;
-}
+// function bilPrima(n){
+//     if(n<=1){
+//         return false;
+//     }
+//     for (let i=2; i<=n-1;i++){
+//         if (n % i === 0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
-while (lowLimit<=highLimit){
-    if (bilPrima(lowLimit)){
-        console.log(lowLimit);
-    }    
-    lowLimit++;
-}
+// while (lowLimit<=highLimit){
+//     if (bilPrima(lowLimit)){
+//         console.log(lowLimit);
+//     }    
+//     lowLimit++;
+// }
     
 
 //console.log(list);
+
+//MATERI LATIHAN P0W2D3
+
+var arr = [1, 2, 15];
+var arrChar = ['Tono', 'Budi', 'Charlie', 'Ahmad']
+
+//Push()
+// arr.push(4);
+
+//Pop()
+// arr.pop();
+
+//UnShift()
+// arr.unshift(3);
+
+//shift()
+// arr.shift();
+
+//sort()
+// arr.sort();
+// arrChar.sort();
+// arr.sort(function(value1, value2){return value1 > value2});
+
+//splice()
+// var arr1 = ['buku', 'laptop', 'komputer'];
+
+// arr1.splice(2, 0, 'televisi'); //mulai dari index 2, menghapus 0 data, menambahkan nilai televisi
+// console.log(arr1);
+
+// arr1.splice(0, 2); //mulai dari index 0, menghapus 2 data
+// console.log(arr1);
+
+// arr1.splice(0, 1, 'majalah', 'koran'); //mulai dari index 0, menghapus 1 data, menambahakan majalah dan koran
+// console.log(arr1);
+
+//split()
+// var kalimat = 'saya adalah full-stack javascript programmer!';
+// var kata = kalimat.split(' ');
+// console.log(kata);
+
+// console.log(arr);
+// console.log(arrChar);
+
+//object()
+const supermanObj = {
+    id: '001',
+    name: 'superman',
+    address: 'Cryton street',
+    age: 32,
+    stillAlive: true,
+    hobbies: [
+        'saving planet earth',
+        'reading newspaper',
+    ],
+    favoriteFoods: [
+        'bakso granat',
+        'pecel lele'
+    ]
+}
+
+const person = {
+    name: 'John',
+    age: 23
+}
+
+// property re assignment
+
+person.name = 'Mike';
+person.age = 40;
+
+//modular function
+
